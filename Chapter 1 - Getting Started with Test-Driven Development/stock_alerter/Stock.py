444a25bc829dbd4ce9646357e29c992397a7e234
Miguel Rentes <miguel.rentes@gmail.com>
2015-05-26 18:21:18 +0100

    Makes the first test pass

--- Chapter 1 - Getting Started with Test-Driven Development/stock_alerter/Stock.py
+++ Chapter 1 - Getting Started with Test-Driven Development/stock_alerter/Stock.py
@@ -4,6 +4,13 @@
 import unittest
 
 
+class Stock:
+    """The Stock class"""
+    def __init__(self, symbol):
+        self.symbol = symbol
+        self.price = None
+
+
 class StockTest(unittest.TestCase):
     """Tests that a price of a new Stock should be None"""
     def test_price_of_a_new_stock_class_should_be_None(self):
