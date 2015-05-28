bac0079369378af525c73e590d16586b3c231164
Miguel Rentes <miguel.rentes@gmail.com>
2015-05-28 19:07:12 +0100

    Adds the setUp method override so all tests methods call this method before the actual execution of the test

--- Chapter 2 - Red-Green-Refactor - The TDD Cycle/stock_alerter/tests/test_stock.py
+++ Chapter 2 - Red-Green-Refactor - The TDD Cycle/stock_alerter/tests/test_stock.py
@@ -8,29 +8,31 @@ from datetime import datetime
 
 class StockTest(unittest.TestCase):
     """The Stock Test suite"""
+    def setUp(self):
+        """This method is called before every test method below is executed"""
+        self.goog = Stock("GOOG")
+
     def test_price_of_a_new_stock_class_should_be_None(self):
         """Tests that a price of a new Stock should be None"""
-        stock = Stock("GOOG")
-        self.assertIsNone(stock.price)
+        self.assertIsNone(self.goog.price)
 
     def test_stock_update(self):
         """An update should set the price on the stock object
         We will be using the `datetime` module for the timestamp
         """
         # Following the Arrange-Act-Assert pattern
-        goog = Stock("GOOG")  # Arrange
-        goog.update(datetime(2015, 5, 28), price=10)  # Act
-        self.assertEqual(10, goog.price)  # Assert
+        # Arrange is in the setUp method above
+        self.goog.update(datetime(2015, 5, 28), price=10)  # Act
+        self.assertEqual(10, self.goog.price)  # Assert
 
     def test_negative_price_should_throw_ValueError(self):
         """Tests that a price should not be negative
         """
-        goog = Stock("GOOG")
-        self.assertRaises(ValueError, goog.update, datetime(2015, 5, 28), -1)
+        self.assertRaises(ValueError, self.goog.update,
+                          datetime(2015, 5, 28), -1)
 
     def test_stock_price_should_give_the_latest_price(self):
         """After multiple updates, a Stock gives us the latest price"""
-        goog = Stock("GOOG")
-        goog.update(datetime(2015, 5, 28), price=10)
-        goog.update(datetime(2015, 5, 28), price=8.4)
-        self.assertAlmostEqual(8.4, goog.price, delta=0.0001)
+        self.goog.update(datetime(2015, 5, 28), price=10)
+        self.goog.update(datetime(2015, 5, 28), price=8.4)
+        self.assertAlmostEqual(8.4, self.goog.price, delta=0.0001)
