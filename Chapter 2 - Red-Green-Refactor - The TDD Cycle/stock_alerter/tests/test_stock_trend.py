7454aca33fea9349565307adcc328474cbdf20ab
Miguel Rentes <miguel.rentes@gmail.com>
2015-05-29 15:42:00 +0100

    Refactors the Stock Trend class, adding a helper method to keep the DRY principle

--- Chapter 2 - Red-Green-Refactor - The TDD Cycle/stock_alerter/tests/test_stock_trend.py
+++ Chapter 2 - Red-Green-Refactor - The TDD Cycle/stock_alerter/tests/test_stock_trend.py
@@ -12,29 +12,24 @@ class StockTrendTest(unittest.TestCase):
         """This method is called before every test method below is executed"""
         self.goog = Stock("GOOG")
 
-    def test_increasing_trend_is_true_if_price_increase_for_3_updates(self):
-        """Checks if a stock has an increasing trend"""
+    def given_a_series_of_prices(self, prices):
+        """Helper method to keep our tests DRY"""
         timestamps = [datetime(2015, 5, 28), datetime(2015, 5, 29),
                       datetime(2015, 5, 30)]
-        prices = [8, 10, 12]
         for timestamp, price in zip(timestamps, prices):
             self.goog.update(timestamp, price)
+
+    def test_increasing_trend_is_true_if_price_increase_for_3_updates(self):
+        """Checks if a stock has an increasing trend"""
+        self.given_a_series_of_prices([8, 10, 12])
         self.assertTrue(self.goog.is_increasing_trend())
 
     def test_increasing_trend_is_false_if_price_decreases(self):
         """Checks that increasing trend is false if price decreases"""
-        timestamps = [datetime(2015, 5, 28), datetime(2015, 5, 29),
-                      datetime(2015, 5, 30)]
-        prices = [8, 12, 10]
-        for timestamp, price in zip(timestamps, prices):
-            self.goog.update(timestamp, price)
+        self.given_a_series_of_prices([8, 12, 10])
         self.assertFalse(self.goog.is_increasing_trend())
 
     def test_increasing_trend_is_false_if_price_equal(self):
         """Checks that increasing trend is false if price remains equal"""
-        timestamps = [datetime(2015, 5, 28), datetime(2015, 5, 29),
-                      datetime(2015, 5, 30)]
-        prices = [8, 10, 10]
-        for timestamp, price in zip(timestamps, prices):
-            self.goog.update(timestamp, price)
+        self.given_a_series_of_prices([8, 10, 10])
         self.assertFalse(self.goog.is_increasing_trend())
